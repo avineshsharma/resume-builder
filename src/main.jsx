7cd297f2bab3a31b8b04d98b11ce5f04dd5dfe21
avineshsharma <jangraavinesh@gmail.com>
2025-03-14 00:39:11 +0530

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   document.getElementById('root')
+// );
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root')); // Create root once
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
