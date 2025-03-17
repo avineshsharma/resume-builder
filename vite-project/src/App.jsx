12ecc0765886176c740b9a776b0f841e49486068
avineshsharma <jangraavinesh@gmail.com>
2025-03-17 16:04:40 +0530

    second

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,29 +1,33 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 // import './App.css'
 // import App1 from "./App1"
-function App () {
-  let counter = 5 
+function App() {
 
-   const addvalue=()=>
-    {
-      // counter = counter +1
-      console.log("value-add",counter )
+  let [counter, setcounter] = useState(5)
+  // let counter = 5 
 
-    } 
-    const removevalue =()=>{
-      console.log ("remove-value",Math.random())
-    }
+  const addvalue = () => {
+    // counter = counter + 1
+    setcounter(counter+1)
+    console.log("value-add", counter)
+
+  }
+  const removevalue = () => {
+    // counter = counter - 1
+    setcounter(counter-1)
+    console.log("remove-value", counter)
+  }
   return (
     <>
-    <h1>1 day</h1>
-    <h2>working and count value :: {counter} </h2>
+      <h1>1 day</h1>
+      <h2>working and count value :: {counter} </h2>
 
-    <button onClick={addvalue}>
-      add value{counter} </button>
-    <br />
-    <button onClick={removevalue}>
-      remove value{counter}</button>
+      <button onClick={addvalue}>
+        add value{counter} </button>
+      <br />
+      <button onClick={removevalue}>
+        remove value{counter}</button>
     </>
     //  <App/> 
   )
